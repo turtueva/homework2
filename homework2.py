6c027c25c2e46f694e11c358662569924395f8af
turtueva <turtueva.medegma@yandex.ru>
2024-07-16 00:01:25 +0800

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -2,5 +2,5 @@ Number_of_completed_task="12" #Всего задач:
 Number_of_hours_spend="1.5"  #Затрачено часов:
 Course_name="Python" # Курс
 Time_to_complete_one_task="0.125 часа" #Среднее время выполнения
-print('Курс:',Course_name,',','всего задач:', Number_of_completed_task,',','затрачено часов:',Number_of_hours_spend,',','среднее время выполнения',Time_to_complete_one_task)
+print('Курс:',Course_name+',','всего задач:', Number_of_completed_task+',','затрачено часов:',Number_of_hours_spend+',','среднее время выполнения',Time_to_complete_one_task)
 
